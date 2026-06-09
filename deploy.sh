696ca6bfd0bc0786502ed068bbcf57d3deeb8912
Moka <moka@MokadeMacBook-Pro.local>
2026-06-09 16:52:47 +0800

    fix: 引入 lz-string 压缩存储，解决 localStorage 配额超限导致合并同步报错的问题

--- deploy.sh
+++ deploy.sh
@@ -48,6 +48,7 @@ files = {
     'echarts.min.js': 'application/javascript',
     'xlsx.full.min.js': 'application/javascript',
     'cos-js-sdk-v5.min.js': 'application/javascript',
+    'lz-string.min.js': 'application/javascript',
 }
 
 for filename, content_type in files.items():
